c507e1f2e27c028cc7e96c99bafcb865a9e781af
Fernanda Ramos <fernandaramos.a@outlook.com>
2020-03-31 16:01:22 -0300

    Adicona uma template engine

--- server.js
+++ server.js
@@ -1,14 +1,51 @@
 const express = require("express")
 const server = express()
 
+const ideas = [
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+    title: "Cursos de programação",
+    category: "Estudo",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae inventore reiciendis laboriosam nesciunt accusamus quo delectus repudiandae",
+    url: "https://www.google.com/"
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+    title: "Exercícios",
+    category: "Saúde",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae inventore reiciendis laboriosam nesciunt accusamus quo delectus repudiandae",
+    url: "https://www.google.com/"
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+    title: "Meditação",
+    category: "Mentalidade",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae inventore reiciendis laboriosam nesciunt accusamus quo delectus repudiandae",
+    url: "https://www.google.com/"
+  },
+  {
+    img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+    title: "Karaokê",
+    category: "Diversão em Família",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae inventore reiciendis laboriosam nesciunt accusamus quo delectus repudiandae",
+    url: "https://www.google.com/"
+  },
+]
+
 server.use(express.static("public"))
 
+const nunjucks = require("nunjucks")
+nunjucks.configure("views", {
+  express: server,
+  noCache: true
+})
+
 server.get("/", function (req, res) {
-  return res.sendFile(__dirname + "/index.html")
+  return res.render("index.html", { ideas })
 })
 
 server.get("/ideias", function (req, res) {
-  return res.sendFile(__dirname + "/ideias.html")
+  return res.render("ideias.html")
 })
 
 server.listen(3000)
