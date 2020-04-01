3396d897aa4f1fd34073e29bd028b854a45cab05
Fernanda Ramos <fernandaramos.a@outlook.com>
2020-04-01 16:22:10 -0300

    Ajusta a inserção do banco de dados

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ const db = require("./db")
 // ]
 
 server.use(express.static("public"))
+server.use(express.urlencoded({ extended: true }))
 
 const nunjucks = require("nunjucks")
 nunjucks.configure("views", {
@@ -85,6 +86,35 @@ server.get("/ideias", function (req, res) {
     return res.render("ideias.html", { ideas: reversedIdeas })
   })
 
+})
+
+server.post("/", function (req, res) {
+  const query = `
+    INSERT INTO ideas(
+      image,
+      title,
+      category,
+      description,
+      link
+    ) VALUES (?, ?, ?, ?, ?);
+  `
+
+  const values = [
+    req.body.image,
+    req.body.title,
+    req.body.category,
+    req.body.description,
+    req.body.link,
+  ]
+
+  db.run(query, values, function (err) {
+    if (err) {
+      console.log(err)
+      return res.send("Erro no banco de dados!")
+    }
+
+    return res.redirect("/ideias")
+  })
 })
 
 server.listen(3000)
